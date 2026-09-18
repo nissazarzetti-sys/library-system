8b37fd398f682128586b098ac4afd93aa20e9ad7
nissazarzetti-sys <nissazarzetti@gmail.com>
2026-09-19 00:21:17 +0700

    Add route parameter

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
-use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\MemberController;
+use App\Http\Controllers\DashboardController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/books', [BookController::class, 'index']);
-Route::get('/categories', [CategoryController::class, 'index']);
+
+Route::get('/books/{id}', [BookController::class,'show']);
+
+Route::get('/categories', [CategoriesController::class, 'index']);
+
 Route::get('/members', [MemberController::class, 'index']);
+
+Route::get('/members/{id}', [MemberController::class, 'show']);
+
+Route::get('/dashboard', [DashboardController::class, 'index']);
