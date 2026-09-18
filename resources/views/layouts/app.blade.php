3938ea2a390c621634b511e9fa9c506356728be9
nissazarzetti-sys <nissazarzetti@gmail.com>
2026-09-19 00:20:52 +0700

    Add blade layout and navigation

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <title>
+        @yield('title')
+    </title>
+</head>
+<body>
+    <header>
+        <h1>Library System</h1>
+
+        <nav>
+            <a href="/dashboard">Dashboard</a>
+            <a href="/books">Books</a>
+            <a href="/categories">Categories</a>
+            <a href="/members">Members</a>
+        </nav>
+        <hr>
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+
+    <footer>
+        <hr>
+        <p>Library System</p>
+    </footer>
+</body>
+</html>
